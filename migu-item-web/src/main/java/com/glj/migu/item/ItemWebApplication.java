efb9be6fa6875bba6bec4adcf7448a769a033b11
guolongjie <1084665773@qq.com>
2019-11-08 15:17:08 +0800

    redis

---
+++ migu-item-web/src/main/java/com/glj/migu/item/ItemWebApplication.java
@@ -0,0 +1,13 @@
+package com.glj.migu.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemWebApplication.class, args);
+    }
+
+}
